7ca11ce2ec156ab4db53d475eb8930ce8478b660
Michelfrancis Bustillos <michelfrancisb@gmail.com>
2020-08-29 23:32:37 -0400

    Update C-P01.

--- C-P01/C-P01/Source.cpp
+++ C-P01/C-P01/Source.cpp
@@ -1,10 +1,105 @@
 #pragma warning(disable:4996)
 #include <stdio.h>
 #include <conio.h>
+#include <ctype.h>
+
+#ifndef __OPERATORS__
+	#define MULTIPLY 1
+	#define DIVIDE 2
+	#define ADD 3
+	#define SUBTRACT 4
+	#define MODULUS 5
+	#define UNSET 6
+#endif
 
 int main() {
+	//Delcare Variables
+	int firstNumber = 0, secondNumber = 0, operationFlag = UNSET;
+	float answer = 0;
+	char operation = ' ';
 
+
+	//Get User Input
+	printf("Enter a number between -1000 and 1000: ");
+	scanf("%d", &firstNumber);
+	rewind(stdin);
+
+	while (firstNumber > 1000 || firstNumber < -1000) { //Ensure input is in range, otherwise prompt again
+		printf("That number was out of range (-1000 - 1000). Please try again: ");
+		scanf("%d", &firstNumber);
+		rewind(stdin);
+	}
+
+	printf("Enter another number between -1000 and 1000: ");
+	scanf("%d", &secondNumber);
+	rewind(stdin);
+
+	while (secondNumber > 1000 || secondNumber < -1000) { //Ensure input is in range, otherwise prompt again
+		printf("That number was out of range (-1000 - 1000). Please try again: ");
+		scanf("%d", &secondNumber);
+		rewind(stdin);
+	}
+
+	printf("Enter the symbol for the operation you would like to perform with the given numbers (*, /, -, +, %): ");
+	scanf("%c", &operation);
+	rewind(stdin);
+
+	//Process Entered Operation
+	if (operation == '+') {
+		operationFlag = ADD;
+	}
+	else if (operation == '-') {
+		operationFlag = SUBTRACT;
+	}
+	else if (operation == '/') {
+		operationFlag = DIVIDE;
+	}
+	else if (operation == '*') {
+		operationFlag = MULTIPLY;
+	}
+	else if (operation == '%') {
+		operationFlag = MODULUS;
+	}
+	else { //Return an error if unknown operation is chosen
+		printf("ERROR: Unknown Operation!"); 
+		_getch();
+		return 0;
+	}
+
+	//Do Maths
+	switch (operationFlag) {
+	case ADD:
+		answer = firstNumber + secondNumber;
+		break;
+	case SUBTRACT:
+		answer = firstNumber - secondNumber;
+		break;
+	case MULTIPLY:
+		answer = firstNumber * secondNumber;
+		break;
+	case DIVIDE:
+		while (secondNumber == 0) { //Check for divide by zero & repromt if necessary
+			printf("ERROR: Cannot divide by zero!\n");
+			printf("Enter another number between -1000 and 1000: ");
+			scanf("%d", &secondNumber);
+			rewind(stdin);
+		}
+		answer = (float)firstNumber / (float)secondNumber;
+		break;
+	case MODULUS:
+		answer = firstNumber % secondNumber;
+		break;
+	}
+
+	//Output Results
+	printf("%d %c %d = %.2f\n", firstNumber, operation, secondNumber, answer);
 
 	_getch();
 	return 0;
 }
+
+//  9 %  2 =  1.00
+// -9 %  2 = -1.00
+//  9 % -2 = -1.00
+// -9 % -2 = -1.00
+// Modulus equations containing a negative will always return a negative answer. Equations containing no negatives will always return a positive answer.
--- HelloWorld/HelloWorld/Source.cpp
+++ HelloWorld/HelloWorld/Source.cpp
@@ -1,24 +1,27 @@
-/*
-Author: Mark Dencler
-Description: Prints out the extended ASCII representations on the system.
-Other: Extended ASCII listing:
-	   http://www.cdrummond.qc.ca/cegep/informat/Professeurs/Alain/files/ascii.htm
-*/
-
+#pragma warning(disable:4996)
 #include <stdio.h>
 #include <conio.h>
 
 int main() {
-	int i = 0;
+	char signedChar = 32;				// also 'signed char'
+	unsigned char unsignedChar = 32;
 
-	// print title line
-	printf(" DEC  | HEX  | CHAR\n");
+	printf("Please enter a single character: ");
+	scanf("%c", &signedChar);
 
-	// print out ascii lines
-	for (i = 32; i <= 255; i++) {
-		printf(" %-5d| %-5x| %-5c\n", i, i, i);
-	}
+	// Both variables use the data in the byte the exact same way.
+	unsignedChar = signedChar;
 
+	// The difference in the data type causes the effective
+	//   ranges of the variables to be different.
+	// 1.) The signed 'char' goes from -128 to 127.
+	// 2.) The 'unsigned char' goes from 0 to 255.
+	// 3.) Both the 'signed char' and 'unsigned char' have
+	//       8 bits of data (1 byte).
+	printf("signed value  : %d\n", signedChar);
+	printf("unsigned value: %d\n", unsignedChar);
+
+	// Return to OS
 	_getch();
 	return 0;
 }
